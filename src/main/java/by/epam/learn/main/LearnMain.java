b1899ee188c863fa8881307fe5be9991c0d3ef31
NaSkvo <naskvo@gmail.com>
2021-12-08 22:18:07 +0100

    about clean code added.
    smth added to main long time ago.

--- src/main/java/by/epam/learn/main/LearnMain.java
+++ src/main/java/by/epam/learn/main/LearnMain.java
@@ -1,9 +1,15 @@
 package by.epam.learn.main;
 
 public class LearnMain {
+
+    public static int maxxx(int ...a) {
+        return -1;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println(42);
         System.out.println("\n\tJava" + 11);
+        System.out.println(maxxx(1,2,3));
     }
 }
---
+++ src/main/java/cleancode/TestsSyntax.java
@@ -0,0 +1,8 @@
+package cleancode;
+
+public class TestsSyntax {
+    //[Test]
+    // @Test
+    //method name
+    //Assert.AssertFalse(el.Enabled);
+}
